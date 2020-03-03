3249f95229550ceddef642b709269dd1b3b7a682
joongsim <joonjooly@gmail.com>
2020-03-02 22:30:47 -0800

    First commit

---
+++ hopkins.py
@@ -0,0 +1,25 @@
+# libraries
+import numpy as np
+import pandas as pd     # for DataFrame and read_excel
+
+from random import randint, uniform
+from sklearn.neighbors import NearestNeighbors
+
+def hopkins(data, N, reps = 20):
+    # 1. Pick n points from dataset at random
+    n = []
+    for i in range(N):
+        n.append(randint(0, data.shape[0] - 1))
+
+
+
+
+def main():
+    print('hi')
+
+
+
+
+
+if __name__ == "__main__":
+    main()
